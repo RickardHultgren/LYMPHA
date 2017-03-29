e933b6dbc8c89bf73dbc234593c13da2f5e62281
Rickard Hultgren <sababa.sababa@gmail.com>
2017-03-29 08:21:43 +0200

    Add files via upload

--- footer.js
+++ footer.js
@@ -1,4 +1,4 @@
-document.write("<div id=\"footer\" style=\"margin: 2em auto 0em auto;width:100%;border:0em; border-top:2px; border-style:solid; border-color:#777777;font-size:100%;padding:1em 0em 0em 0em;width:810px;float:left;");
+document.write("<div id=\"footer\" style=\"border-top:0.2vw solid #777777;padding:1em 0em 0em 0em;float:left;");
 document.write("	<a class=\"linking\" rel=\"license\" href=\"http:\/\/creativecommons.org\/licenses\/by\/3.0\/\"><img alt=\"Creative Commons License\" style=\"border-width:0\" src=\"http:\/\/i.creativecommons.org\/l\/by\/3.0\/80x15.png\" \/><\/a> Copyright &copy; Rickard Verner Hultgren<br>This work is licensed under a <a rel=\"license\" class=\"linking\" href=\"http:\/\/creativecommons.org\/licenses\/by\/3.0\/\">Creative Commons Attribution 3.0 Unported License<\/a>.");
 document.write("<\/div> ");
 document.write("");
--- layout.js
+++ layout.js
@@ -6,28 +6,28 @@ function changeMenu() {
 	}
 }
 
-document.write("<font style=\"font-family: \"Merriweather\",\"Droid Serif\", Georgia, 'Times New Roman', Times, serif;\"><div class=\"container\" ");
-document.write("	<div id=\"menubig\" style=\"overflow:hidden;margin: 0px auto 0px auto; text-align:center; border:0em;border-bottom:2px; border-style:solid; border-color:#777777;\" id=\"thetop\">");
-document.write("		<div style=\"float:left;width:auto;width:95%;padding:1em 0px 1em 0px;height:7em;background-size:cover;background-size:150px, auto;background-repeat:no-repeat;margin:0.2em 0px 2px 0px;width:15%; height:auto;\"><a href=\"index.html\" style=\'font-family:\"Share Tech Mono\",\"Lucida Console\",Monospace,\"DejaVu Sans Mono\",\"Courier New\",MiscFixed; font-size:9px;\'><img id=\"logopic\" src=\"logo.png\" style=\"margin:1.8em 0em 0em 0em; float:left;\"><br>");
-document.write("		</a><\/div><br><br><span style=\"font-size:18px;color:#6400EF;margin-left:-2.5em;\">THE LANGUAGE FOR MEDICAL ALGORITHMS</span><br>");
-document.write("		<a id=\"contacttext\" href=\"contact.html\"><div class=\"menu2\" id=\"contact\">CONTACT<\/div><\/a>");
-document.write("		<a id=\"historytext\" href=\"history.html\"><div class=\"menu2\" id=\"history\">HISTORY<\/div><\/a>");
-document.write("		<a id=\"blogtext\" href=\"http://lymphascripting.blogspot.se/\"><div class=\"menu2\" id=\"blog\">BLOG<\/div><\/a>");
-document.write("		<a id=\"downloadstext\" href=\"downloads.html\"><div class=\"menu2\" id=\"downloads\">DOWNLOADS<\/div><\/a>");
+document.write("<font style=\"font-family: \"Merriweather\",\"Droid Serif\", Georgia, 'Times New Roman', Times, serif;\"><div class=\"container\" style=\"\"");
+document.write("	<div id=\"menubig\" style=\"overflow:hidden;font-size:1.5vw; text-align:center; \" id=\"thetop\">");
+document.write("		<div style=\"font-size:1.5vw;margin:0vw 20vw 0vw 20vw ;width:60vw; ;float:left; margin-bottom:0.5em;border-bottom:0.1em solid #cccccc;\"><a href=\"index.html\"><img id=\"logopic\" src=\"logo.png\">");
+document.write("		<\/a><span style=\"float:right;display:block;padding:2.5em 1.5em 0.5em  0.5em;font-size:0.99em;letter-spacing:0em;color:#6400EF;font-weight:bold;\">BUILDING VALUE IN MEDICAL REASONING<\/span>");
+document.write("	<div style=\"border-top:0.2vw solid #cccccc;text-align:center;z-index:3;\">");
+document.write("		<a id=\"indextext\" href=\"index.html\"><div class=\"menu2\" id=\"index\">HOME<\/div><\/a> ");
 document.write("		<a id=\"docstext\" href=\"docs.html\"><div class=\"menu2\" id=\"docs\">DOCS<\/div><\/a>");
-document.write("		<a id=\"indextext\" href=\"index.html\"><div class=\"menu2\" id=\"index\">HOME<\/div><\/a>");
-document.write("	<\/div> ");
-document.write("<\/div> ");
-document.write("<div id=\"menusmall\" style=\"text-align:center;border-style: solid;border-bottom: thick solid #aaaaaa;\">");
-document.write("	<img id=\"menubutton\" src=\"logo.png\" alt=\"LYMPHA\" style=\"height:auto;width:50%;max-width:250px;\" onclick=\"changeMenu();\">");
-document.write("	<img id=\"menubutton\" src=\"menu.png\" alt=\"MENU\" style=\"height:auto;width:25%;max-width:84px;\" onclick=\"changeMenu();\">");
-document.write("	<span class=\"subtitle\" style=\"background-color:#ffffff; font-size:18px;color:#6400EF;margin-left:-2.5em;\">THE LANGUAGE FOR MEDICAL ALGORITHMS</span>");
-document.write("	<div id=\"menuish\" style=\"width=100%;display:none;\">");
+document.write("		<a id=\"downloadstext\" href=\"downloads.html\"><div class=\"menu2\" id=\"downloads\">DOWNLOADS<\/div><\/a>");
+document.write("		<a id=\"blogtext\" href=\"http://lymphascripting.blogspot.se/\"><div class=\"menu2\" id=\"blog\">BLOG<\/div><\/a>");
+//document.write("		<a id=\"contacttext\" href=\"contact.html\"><div class=\"menu2\" id=\"contact\">CONTACT<\/div><\/a>");
+//document.write("		<a id=\"historytext\" href=\"history.html\"><div class=\"menu2\" id=\"history\">HISTORY<\/div><\/a>");
+document.write("	<\/div>");
+document.write("<\/div><\/div> ");
+document.write("<div id=\"menusmall\" style=\"text-align:center;\">");
+document.write("	<a style=\"vertical-align:top;\" href=\"index.html\"><img src=\"logo.png\" alt=\"LYMPHA\" style=\"height:auto;width:60vw;margin-right:4em;vertical-align:bottom;\" ;\"><\/a>");
+document.write("	<div style=\"width:4em;height:4em;position:absolute;\"> <\/div><div class=\"menubutton\" style=\"font-style:normal;vertical-align:top;color:#EFAB00;margin-bottom:-0em;font-size:20vw;border:none;display:inline-block;position: absolute;right:-.25em;top:-.38em;\" onclick=\"changeMenu();\">&#8801;<\/div><BR>");
+document.write("	<div id=\"menuish\" style=\"width=100%;display:none;position:relative;z-index:1;\">");
 document.write("		<a href=\"index.html\"><div id=\"indexsmall\" class=\"menu2\">HOME<\/div><\/a>");
 document.write("		<a href=\"docs.html\"><div id=\"docssmall\" class=\"menu2\">DOCS<\/div><\/a>");
 document.write("		<a href=\"downloads.html\"><div id=\"downloadssmall\" class=\"menu2\">DOWNLOADS<\/div><\/a>");
 document.write("		<a id=\"blogsmall\" href=\"http://lymphascripting.blogspot.se/\"><div class=\"menu2\" id=\"blog\">BLOG<\/div><\/a>");
-document.write("		<a href=\"history.html\"><div id=\"historysmall\" class=\"menu2\">HISTORY<\/div><\/a>");
-document.write("		<a href=\"contact.html\"><div id=\"contactsmall\" class=\"menu2\">CONTACT<\/div><\/a>");
+//document.write("		<a href=\"history.html\"><div id=\"historysmall\" class=\"menu2\">HISTORY<\/div><\/a>");
+//document.write("		<a href=\"contact.html\"><div id=\"contactsmall\" class=\"menu2\">CONTACT<\/div><\/a>");
 document.write("	<\/div>");
-document.write("<\/div>");
+document.write("<hr style=\"z-index:1;position:relative;\"><\/div>");
