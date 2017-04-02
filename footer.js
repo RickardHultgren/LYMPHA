46d5d97ea369cb7c8e7a2cf2e81d98b9e58426bd
Rickard Hultgren <sababa.sababa@gmail.com>
2017-04-02 10:26:57 +0200

    Delete footer.js

--- footer.js
+++
@@ -1,6 +0,0 @@
-document.write("<div id=\"footer\" style=\"border-top:0.2vw solid #777777;padding:1em 0em 0em 0em;float:left;");
-document.write("	<a class=\"linking\" rel=\"license\" href=\"http:\/\/creativecommons.org\/licenses\/by\/3.0\/\"><img alt=\"Creative Commons License\" style=\"border-width:0\" src=\"http:\/\/i.creativecommons.org\/l\/by\/3.0\/80x15.png\" \/><\/a> Copyright &copy; Rickard Verner Hultgren<br>This work is licensed under a <a rel=\"license\" class=\"linking\" href=\"http:\/\/creativecommons.org\/licenses\/by\/3.0\/\">Creative Commons Attribution 3.0 Unported License<\/a>.");
-document.write("<\/div> ");
-document.write("");
-
-
