28828482243a5da625d5d303c4dfea4b10df8ef6
Rickard Hultgren <sababa.sababa@gmail.com>
2021-08-14 08:03:45 +0000

    react test

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.py$/,
+        loader: 'py-loader',
+        options: {
+          compiler: 'transcrypt'
+        }
+      }
+    ]
+  }
+}
